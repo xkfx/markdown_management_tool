931a03a55735ba57e034ae212c30312192c81274
xkfx <531194294@qq.com>
2021-11-21 17:22:44 +0800

    FileSearch组件

--- preload.js
+++ preload.js
@@ -1,4 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
+    let fileSearch = document.querySelector(".FileSearch");
+    fileSearch.addEventListener("search", () => {
+        if (fileSearch.value != "") {
+            console.log(fileSearch.value);
+        }
+    });   
 })
 
